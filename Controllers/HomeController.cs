6f39f23bee91ab0555fdd8486fb544cfcbfb5889
student <student@DESKTOP-L3ET5DL>
2020-09-07 20:47:03 +0300

    http client step 1

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,12 +1,67 @@
+using System.Runtime.CompilerServices;
+using System.Runtime.Intrinsics.X86;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Security.AccessControl;
+using System;
 using Microsoft.AspNetCore.Mvc;
+using LanguageFeatures.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace LanguageFeatures.Controllers
 {
     public class HomeController : Controller
     {
-        public ViewResult Index() {
+        public async Task<ViewResult> Index() {
             // return View(new string[] { 'C#', 'Language', 'Features' });
-            return View("Index", "C#");
+            // return View("Index", "C#");
+
+            /* return View("Index", Product.GetProducts()
+                .Where(p => p != null)
+                .Select(p => String.Format(
+                    "Name: {0}, Price: {1}, Related Product: {2}",
+                    p.Name,
+                    p.Price,
+                    p.Related?.Name ?? "<None>"
+                    ))
+                .ToList()); */
+                // long? result = await TryGetRemoteData();
+            // if (result != null)
+            string result = await TryGetRemoteData();
+            if (result != null)
+            {
+                return View("Index", new List<string>(){result.ToString()});
+            }
+            return View("Index", new List<string>());
+        }
+
+        // public Task<bool> TryGetRemoteData(out string data) {
+        //     HttpClient client = new HttpClient();
+        //     var httpTask = client.GetAsync(@"http://apress.com");
+        //     return httpTask. ContinueWith<bool>((Task<HttpResponseMessage> response) => {
+        //         try
+        //         {
+        //             data = response.Result.Content.Headers.ContentLength.ToString();
+        //             return true;
+        //         }
+        //         catch (System.Exception)
+        //         {
+        //             return false;
+        //         }
+        //     });
+        // }
+
+        // public Task<long?> TryGetRemoteData() {
+        public Task<string> TryGetRemoteData() {
+                HttpClient client = new HttpClient();
+                var httpTask = client.GetAsync(@"http://apress.com");
+                return httpTask.ContinueWith(async (Task<HttpResponseMessage> response) => {
+                    // return response.Result.Content.Headers.ContentLength;
+                    string result = await await response.Result.Content.ReadAsStringAsync();
+                    return result;
+                });
         }
     }
 }
--- Models/Product.cs
+++ Models/Product.cs
@@ -1,18 +1,25 @@
+using System;
+using System.Collections.Generic;
+
 namespace LanguageFeatures.Models {
 
     public class Product {
 
         public string Name { get; set; }
         public decimal? Price { get; set; }
+        public Product Related { get; set; }
+        public string Category { get; set; } = "Watersports";
+        public bool InStock { get; } = true;
         
-        public static Product[] GetProducts() {
+        public static List<Product> GetProducts() {
             Product kayak = new Product {
                 Name = "Kayak", Price = 275M
             };
             Product lifejacket = new Product {
-                Name = "Lifejacket", Price = 48.95M
+                Name = "Lifejacket", Price = 48.95M, Category = "Water Craft"
             };
-            return new Product[] { kayak, lifejacket, null };
+            kayak.Related = lifejacket;
+            return new List<Product> { kayak, lifejacket, null };
         }
     }
 }
